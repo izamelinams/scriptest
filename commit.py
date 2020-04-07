66b0d8e9c03fdf8ff0ccf9dae04e0d83f26e3740
Iza Melina <izamelina97@gmail.com>
2020-04-07 13:12:01 -0500

    Update the database 1586283121

--- commit.py
+++ commit.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python3
 import subprocess as cmd
+import os
+import time 
 
 cp = cmd.run("git add .", check=True, shell=True)
 #print(cp)
 
-#response = input("Do you want to use the default message for this commit?([y]/n)\n")
-message = "update the repository"
+print("Running")
 
-#if response.startswith('n'):
-#    message = input("What message you want?\n")
-
-cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
+timesp = str(int(time.time()))
+message = "git commit -m 'Update the database {}'".format(timesp)
+cp = cmd.run(message,check=True,shell=True)
+#cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
 cp = cmd.run("git push -u origin master -f", check=True, shell=True)
+print("hi")
